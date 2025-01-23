2dd9576d7eb888a90cf14c9ebf5b251a57d19919
gabrielecorona8989 Corona <89gabbo89@gmail.com>
2025-01-23 08:53:50 +0000

    commit

--- mysql/scuola.sql
+++ mysql/scuola.sql
@@ -2,25 +2,25 @@ CREATE DATABASE IF NOT EXISTS scuola;
 USE scuola; 
  
 CREATE TABLE IF NOT EXISTS corso_studi( 
-    id INT AUTO INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome_corso VARCHAR(50) NOT NULL
 ); 
 
 CREATE TABLE IF NOT EXISTS disciplina ( 
-    id INT AUTO INCREMENT PRIMARY KEY, 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
     nome_disciplina VARCHAR(50) NOT NULL
 ); 
 
 CREATE TABLE IF NOT EXISTS studenti( 
-    matricola INT AUTO INCREMENT PRIMARY KEY, 
+    matricola INT AUTO_INCREMENT PRIMARY KEY, 
     cognome VARCHAR(58) NOT NULL, 
-    data nascita DATE NOT NULL, 
-    corso studi INT NOT NULL, 
+    data_nascita DATE NOT NULL, 
+    corso_studi INT NOT NULL, 
     FOREIGN KEY (corso_studi) REFERENCES corso_studi (id) 
 ); 
 
 CREATE TABLE IF NOT EXISTS valutazioni (
-    id INT AUTO INCREMENT PRIMARY KEY, 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
     matricola INT NOT NULL, 
     data_svolgimento DATE NOT NULL, 
     disciplina INT NOT NULL, 
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS valutazioni (
     FOREIGN KEY (disciplina) REFERENCES disciplina (id)
 );
 
-INSERT INTO corso studi (nome_corso) 
+INSERT INTO corso_studi (nome_corso) 
 VALUES
     ('Informatica'), 
     ('Matematica'), 
@@ -39,9 +39,10 @@ VALUES
 
  INSERT INTO studenti (matricola, cognome, data_nascita, corso_studi) 
  VALUES 
-    (1, 'Zanirato', '1990-05-15', 1), 
-    (2, 'Conti', '1992-08-10', 2),
-    (3, 'Corona', '1995-02-12', 3);
+    (1, 'Zanirato', '2006-06-12', 1), 
+    (2, 'Conti', '2006-05-10', 2),
+    (3, 'Corona', '2007-03-10', 3),
+    (4,'rossi','2004-02-03',4);
  
   INSERT INTO disciplina (nome_disciplina) 
   VALUES
@@ -59,4 +60,18 @@ VALUES
     (2, '2022-02-15', 2, 82), 
     (3, '2022-03-01', 1, 92), 
     (3, '2022-03-15', 2, 88),
-    (1, '2022-04-01', 3, 75);
+    (1, '2022-04-01', 3, 75),
+    (1, '2022-04-15', 4, 87),
+    (2, '2022-05-01', 3, 80),
+    (2, '2022-05-15', 4, 90);
+
+
+
+select s.cognome, v.voto,d.nome_disciplina as materia, YEAR(CURDATE())-YEAR(s.data_nascita) as età
+from studenti s, valutazioni v,disciplina d
+where s.matricola = v.matricola and v.disciplina = d.id and s.cognome like 'C%'
+order by cognome;
+
+select s.cognome
+from studenti s
+where YEAR(CURDATE())-YEAR(s.data_nascita)>=18;
