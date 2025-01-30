42b6dc2bded395d3b5a8c0ed226270461143a6bb
gabrielecorona8989 Corona <89gabbo89@gmail.com>
2025-01-30 08:54:54 +0000

    commit

--- mysql/scuola.sql
+++ mysql/scuola.sql
@@ -1,4 +1,5 @@
-mysql -u root
+-- mysql -u root
+
 CREATE DATABASE IF NOT EXISTS scuola;
 USE scuola; 
  
@@ -6,6 +7,7 @@ CREATE TABLE IF NOT EXISTS corso_studi(
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome_corso VARCHAR(50) NOT NULL
 ); 
+DESC disciplina;
 
 CREATE TABLE IF NOT EXISTS disciplina ( 
     id INT AUTO_INCREMENT PRIMARY KEY, 
@@ -29,6 +31,7 @@ CREATE TABLE IF NOT EXISTS valutazioni (
     FOREIGN KEY (matricola) REFERENCES studenti (matricola), 
     FOREIGN KEY (disciplina) REFERENCES disciplina (id)
 );
+SHOW TABLES;
 
 INSERT INTO corso_studi (nome_corso) 
 VALUES
@@ -37,24 +40,25 @@ VALUES
     ('Letteratura'), 
     ('Architettura'), 
     ('Chimica'); 
+SELECT * FROM corso_studi;
 
- INSERT INTO studenti (matricola, cognome, data_nascita, corso_studi) 
- VALUES 
+INSERT INTO studenti (matricola, cognome, data_nascita, corso_studi) 
+VALUES 
     (1, 'Zanirato', '2006-06-12', 1), 
     (2, 'Conti', '2006-05-10', 2),
     (3, 'Corona', '2007-03-10', 3),
     (4,'rossi','2004-02-03',4);
  
-  INSERT INTO disciplina (nome_disciplina) 
-  VALUES
+INSERT INTO disciplina (nome_disciplina) 
+VALUES
     ('Programmazione'),
     ('Matematica'),
     ('Italiano'), 
     ('Francese'), 
     ('Chimica'); 
   
-  INSERT INTO valutazioni (matricola, data_svolgimento, disciplina, voto) 
-  VALUES
+INSERT INTO valutazioni (matricola, data_svolgimento, disciplina, voto) 
+VALUES
     (1,' 2022-01-01', 1, 85), 
     (1, '2022-01-15', 2, 98),
     (2, '2022-02-01', 1, 78),
@@ -66,8 +70,7 @@ VALUES
     (2, '2022-05-01', 3, 80),
     (2, '2022-05-15', 4, 90);
 
-
-
+-- query 
 select s.cognome, v.voto,d.nome_disciplina as materia, YEAR(CURDATE())-YEAR(s.data_nascita) as età
 from studenti s, valutazioni v,disciplina d
 where s.matricola = v.matricola and v.disciplina = d.id and s.cognome like 'C%'
@@ -77,5 +80,14 @@ select s.cognome
 from studenti s
 where YEAR(CURDATE())-YEAR(s.data_nascita)>=18;
 
-select s.cognome, s.nome, s.data_nascita
-from studenti;
+
+select s.cognome, , s.data_nascita
+from studenti s;
+
+select s.cognome
+from studenti s
+where s.cognome like 'C%';
+
+select v.voto
+from valutazioni v
+where v.matricola='1';
