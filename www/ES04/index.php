e4c1700fe19088ee5d3195c50fea154fe262a037
gabrielecorona8989 Corona <89gabbo89@gmail.com>
2025-04-09 13:36:10 +0000

    commit

---
+++ www/ES04/index.php
@@ -0,0 +1,23 @@
+<!--Pagina di benvenuto che mostri un messaggio di benvenuto e un link per accedere alla pagina di login.-->
+<?php
+    session_start();
+    if(isset($_SESSION['utente'])){
+        $collegamento = "<a href='uscita.php'>Esci</a>";
+    } else {
+        $collegamento = "<a href='accesso.php'>Accedi</a>";
+    }
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pagina di Benvenuto</title>
+    </head>
+    <body>
+        <h1>Benvenuto</h1>
+        <h2>Questa è una pagina di benvenuto.</h2>
+        <a href="riservata.php">Riservata</a><br />
+        <?php echo $collegamento;?>
+    </body>
+</html>
