860260a15b4c97a6b7f07560d236695fe8f2166a
gabrielecorona8989 Corona <89gabbo89@gmail.com>
2025-05-28 13:55:52 +0000

    fsfs

--- www/ES05/ES05-c/connessione.php
+++ www/ES05/ES05-c/connessione.php
@@ -1,8 +1,8 @@
 <?php
 // Costanti per la connessione al database
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'ES05_user');
-define('DB_PASSWORD', 'mia_password');
+define('DB_USERNAME', 'gab');
+define('DB_PASSWORD', 'gabbo');
 define('DB_NAME', 'ES05');
 $html_out = "";
 try {
